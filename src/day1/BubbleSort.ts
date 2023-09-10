7312f7861d88ed109d2ba6b45ea0fb0c5dd56913
nickmasonswe <nickmasonswe@gmail.com>
2023-09-10 19:33:24 -0400

    bubble sort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,14 @@
 export default function bubble_sort(arr: number[]): void {
-
+     //only use of the outside loop is to make sure we go all the way thru the length-1;
+    for(let i = 0; i < arr.length-1; i++) {
+        //the "real" loop for comparing. go thru length minus i bc no need to check already sorted values from previous loops.
+        for(let j = 0; j < arr.length - i; j++) {
+            if(arr[j] > arr[j+1]) {
+                //switch their places with a temp var
+                const temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
 }
